7cbda2830ef9d3bc4888b86f3a03161139dbd9e1
Just-StyX <fjaccobian@gmail.com>
2025-09-05 00:35:54 -0400

    version 1.2 shipped

--- catalog-service/src/main/java/jsl/group/catalog_service/service/Product.java
+++
@@ -1,8 +0,0 @@
-package jsl.group.catalog_service.service;
-
-import java.math.BigDecimal;
-
-public record Product(
-        String productName, BigDecimal productPrice
-) {
-}
--- catalog-service/src/main/java/jsl/group/catalog_service/service/ProductList.java
+++
@@ -1,9 +0,0 @@
-package jsl.group.catalog_service.service;
-
-import java.time.LocalDateTime;
-import java.util.List;
-
-public record ProductList(
-        List<Product> products, LocalDateTime localDateTime
-) {
-}
